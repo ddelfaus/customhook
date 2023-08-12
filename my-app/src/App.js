4a42f49a56cd1b4fdd5b2fb2c7d745febaffc9cb
David Delfaus <ddelfaus@gmail.com>
2023-08-12 18:06:09 -0400

    init

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import DynamicTitle from './DynamicTitle';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <DynamicTitle/>
+    </div>
+  );
+}
+
+export default App;
